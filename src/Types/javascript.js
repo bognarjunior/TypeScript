15915d361026542c26c5105b62159087bc82494c
Bognar Junior <bognar_junior@yahoo.com.br>
2018-09-16 18:44:42 -0300

    JavaScript
    Exemplo TypeScript... Arquivo javascript

---
+++ src/Types/javascript.js
@@ -0,0 +1,15 @@
+let qualquer = 1;
+console.log(qualquer, typeof qualquer );
+qualquer = 'String';
+console.log(qualquer, typeof qualquer );
+qualquer = true;
+console.log(qualquer, typeof qualquer );
+qualquer = [1, 2, 3, 4, 'String', true, 5, 6, 'STRING', false];
+console.log(qualquer, typeof qualquer );
+qualquer = {
+  name: 'Bognar',
+  date: new Date(),
+  article: 'TypeScript',
+  publish: true
+};
+console.log(qualquer, typeof qualquer );
