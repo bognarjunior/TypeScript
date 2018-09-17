a87c93a02027f70bf325c1bfb259b487f6e95181
Bognar Junior <bognar_junior@yahoo.com.br>
2018-09-16 22:45:51 -0300

    Tipos em TypeScript
    Exemplos de tipos de dados em TypeScript

--- src/Types/types.js
+++ src/Types/types.js
@@ -1,1 +1,71 @@
+/**
+ * Tipo String
+ */
+var text = 'Valor da string';
+text = 'Mudei o valor';
+// Indicará erro
+//text = 3;
+/**
+ * Tipo Number
+ */
+var num;
+num = 4;
+num = 5 + 8;
+var anotherNum = 5.8;
+var hexNum = 0xf00d;
+var binaryNum = 10;
+var octalNum = 484;
+/**
+ * Boolean
+ */
+var isValid = true;
+isValid = false;
+/**
+ * Tipo de Array
+ */
+var arr = ['Arroz', 'Feijão', 'Couve', 'Bisteca de Porco'];
+var arrGeneric = ['Arroz', 'Feijão', 'Couve', 'Bisteca de Porco'];
+/**
+ * Tupla
+ */
+var tuple;
+tuple = ['hello', 1, 'world', 2];
+console.log(tuple[0]);
+console.log(tuple[1]);
+// Indicará erro
+//tuple = [1, 'hello', 2, 'world'];
+/**
+ * Enum
+ */
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+    Color[Color["Yellow"] = 3] = "Yellow";
+})(Color || (Color = {}));
+;
+var AnotherColor;
+(function (AnotherColor) {
+    AnotherColor[AnotherColor["Red"] = 1] = "Red";
+    AnotherColor[AnotherColor["Green"] = 2] = "Green";
+    AnotherColor[AnotherColor["Blue"] = 4] = "Blue";
+    AnotherColor[AnotherColor["Yellow"] = 5] = "Yellow";
+})(AnotherColor || (AnotherColor = {}));
+;
+console.log(Color);
+/**
+ * Any
+ */
+var qualquer;
+qualquer = 'String';
+qualquer = 4;
+qualquer = true;
+/**
+ * Void
+ */
+function print(msg) {
+    console.log("Fun\u00E7\u00E3o sem retorno: " + msg);
+}
+print('Escreva algo na tela');
 //# sourceMappingURL=types.js.map
---
+++ src/Types/types.ts
@@ -0,0 +1,72 @@
+/**
+ * Tipo String
+ */
+let text: string = 'Valor da string';
+text = 'Mudei o valor';
+
+// Indicará erro
+//text = 3;
+
+/**
+ * Tipo Number
+ */
+let num: number;
+num = 4;
+num = 5 + 8;
+
+let anotherNum: number = 5.8;
+let hexNum: number = 0xf00d;
+let binaryNum: number = 0b1010;
+let octalNum: number = 0o744;
+
+/**
+ * Boolean
+ */
+let isValid: boolean = true;
+isValid = false;
+
+/**
+ * Tipo de Array
+ */
+
+let arr: string[] = ['Arroz', 'Feijão', 'Couve', 'Bisteca de Porco'];
+
+let arrGeneric: Array<string> = ['Arroz', 'Feijão', 'Couve', 'Bisteca de Porco'];
+
+/**
+ * Tupla
+ */
+let tuple: [string, number, string, number];
+tuple = ['hello', 1, 'world', 2];
+
+console.log(tuple[0]);
+console.log(tuple[1]);
+
+// Indicará erro
+//tuple = [1, 'hello', 2, 'world'];
+
+/**
+ * Enum
+ */
+enum Color {Red, Green, Blue, Yellow};
+enum AnotherColor {Red = 1, Green = 2, Blue = 4, Yellow};
+console.log(Color);
+
+/**
+ * Any
+ */
+
+let qualquer: any;
+qualquer = 'String';
+qualquer = 4;
+qualquer = true;
+
+/**
+ * Void
+ */
+
+function print(msg: string ): void {
+  console.log(`Função sem retorno: ${msg}`);
+}
+
+print('Escreva algo na tela');
